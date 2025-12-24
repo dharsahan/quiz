8be02914651ac08bd1d0d5dc49f35882266d1e81
kalaiwebxd07-dotcom <kalaiwebxd07@gmail.com>
2025-12-24 21:18:03 +0530

    feat: Update quiz questions, set `index.html` as the default root, and add an admin login link.

--- server.js
+++ server.js
@@ -354,7 +354,7 @@ const server = http.createServer(async (req, res) => {
     }
 
     // Serve static files
-    let filePath = req.url === '/' ? '/login.html' : req.url;
+    let filePath = req.url === '/' ? '/index.html' : req.url;
     filePath = path.join(__dirname, filePath);
 
     const ext = path.extname(filePath);
